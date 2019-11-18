65d7deac58b6826349ccc67520175dd44eef50c9
Chris Chitla <chrischitla@gmail.com>
2019-11-17 22:06:56 -0700

    created basic page layout

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,8 +3,15 @@ let express = require("express");
 let router = express.Router();
 
 router.get("/", (req, res) => {
+    // call burger.selectAll function then send result to render obj
     res.render("index", {});
-})
+});
+
+// router.post and call burger.insertOne;
+    //add burger directs here
+
+//router.put and call burger.updateOne; 
+    //devour button directs here
+
 
 module.exports = router;
-
