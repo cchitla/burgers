9bf5b2563c9768bab758cb9716d22408d773defa
Chris Chitla <chrischitla@gmail.com>
2019-11-21 21:48:45 -0700

    no idea

--- db/schema.sql
+++ db/schema.sql
@@ -4,8 +4,9 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers (
-    id INT AUTO_INCREMENT,
+    id INT AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(255),
     devoured BOOLEAN,
+    createdAt TIMESTAMP NOT NULL,
     PRIMARY KEY (id)
 );
